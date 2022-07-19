8c6b8d96dad76974636e5cc8fba335af29b22339
Solafa khaled <46757940+solafakhaled@users.noreply.github.com>
2022-07-19 07:39:14 +0300

    Support multi reducers

--- src/Redux/index.js
+++ src/Redux/index.js
@@ -1,25 +1,7 @@
-import { createStore } from "redux";
-
-const counterReducer = (state = { value: 0, toggle: true }, action) => {
-  if (action.type === "increase") {
-    return {
-      ...state,
-      value: state.value + action.payload,
-    };
-  } else if (action.type === "decrease") {
-    return {
-      ...state,
-      value: state.value - action.payload,
-    };
-  } else if (action.type === "toggle") {
-    return {
-      ...state,
-      toggle: !state.toggle,
-    };
-  }
-
-  return state;
-};
-
-const store = createStore(counterReducer);
+import { configureStore } from "@reduxjs/toolkit";
+import CounterSlice from "./CounterSlice";
+import ToggleSlice from "./ToggleSlice";
+const store = configureStore({
+  reducer: { counter: CounterSlice, toggle: ToggleSlice },
+});
 export default store;
